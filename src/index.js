ddb496f5248177f320ed4357c57b979f22f27721
Ignacio Ainol <iggnaxios@gmail.com>
2022-09-19 21:12:02 -0300

    socket.io setup

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
-import express from 'express';
-console.log('hello world');
+import app from './app';
+import { Server as WebsocketServer } from 'socket.io';
+import http from 'http';
+
+const server = http.createServer(app)
+const httpServer = server.listen(3000);
+const io = new WebsocketServer(httpServer);
+
+console.log('Server is runing on port 3000');
